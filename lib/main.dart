a9ad4e96886fb5c7858056f56be2a63970b4052f
amarolahy <ramah.samuel@gmail.com>
2026-08-01 15:57:53 +0300

    Fix : intl pour les dates non initialisé

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/router.dart';
@@ -8,6 +9,7 @@ import 'features/game/application/game_providers.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('fr_FR');
   final prefs = await SharedPreferences.getInstance();
   runApp(
     ProviderScope(
